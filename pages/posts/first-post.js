22d7fabe79321a335434b4ba1f02b8d95886aa54
Park Giram <kirammida@hanyang.ac.kr>
2022-05-13 00:58:55 +0900

    first commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,14 @@
-export default function FirstPost() {
-    return <h1>My Name is Park Gi Ram</h1>;
+import Link from 'next/link';
+
+export default function FirstPost() 
+{
+    return (
+    <><h1>My Name is Park Gi Ram</h1>
+    <h2>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </h2>
+    </>);
   }
   
